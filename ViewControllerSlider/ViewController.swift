254871aa202629c6a403aac49fb5c469cecfb9be
Aydin Unal [Uygulama Gelistirme - Mobil Bankacilik Uygulamalari Bolumu] <Aydin.Unal@ingbank.com.tr>
2019-02-28 12:03:58 +0300

    Initial Commit

---
+++ ViewControllerSlider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewControllerSlider
+//
+//  Created by Aydin Unal [Uygulama Gelistirme - Mobil Bankacilik Uygulamalari Bolumu] on 28.02.2019.
+//  Copyright © 2019 Aydin Unal [Uygulama Gelistirme - Mobil Bankacilik Uygulamalari Bolumu]. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
